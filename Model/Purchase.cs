c1727ad256f5855cbea9c99ec98f601a6da61a37
Nathan <nathan_cordeiro03@hotmail.com>
2022-03-29 14:23:37 -0300

    Adicionando as outras alterações

--- Model/Purchase.cs
+++ Model/Purchase.cs
@@ -2,8 +2,10 @@ namespace Model;
 
 public class Purchase
 {
-   private DateTime date_purchase;
-   private String payment; 
-   private int number_confirmation;
-   private String number_nf;
+    private DateTime date_purchase;
+    private String payment; 
+    private int number_confirmation;
+    private String number_nf;
+    private Client client;
+    List<Purchase> purchases;
 }
