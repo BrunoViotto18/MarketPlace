430e8e3a77b29c6f4f3c39bd5fb5b7c9d1c69c53
Bruno Viotto <brunoviottosantos@gmail.com>
2022-03-29 16:19:04 -0300

    Criação dos GET e SET das classes Purchase e Wishlist e implementação do método addProductToWishList da classe Wishlist.

--- Model/Wishlist.cs
+++ Model/Wishlist.cs
@@ -5,8 +5,28 @@ public class Wishlist
     private Client client;
     List<Product> products; 
 
-    public void addProductToWIshList(Product product)
+    public Wishlist(Client client)
     {
+        this.products = new List<Product>();
+        this.client = client;
+    }
 
+    public Client getClient()
+    {
+        return client;
+    }
+    public void setClient(Client client)
+    {
+        this.client = client;
+    }
+
+    public List<Product> getProducts()
+    {
+        return products;
+    }
+
+    public void addProductToWishList(Product product)
+    {
+        products.Add(product);
     }
 }
