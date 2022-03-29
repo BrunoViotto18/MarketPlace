3bb8cc8008d72e0de56f4b7cb24173fe616e917d
Bruno Viotto <brunoviottosantos@gmail.com>
2022-03-29 15:40:51 -0300

    Criação do GET e SET e construtores das classes Address, Person e Store

--- Model/Person.cs
+++ Model/Person.cs
@@ -2,11 +2,7 @@
 
 public class Person
 {
-    protected Person(Address address)
-    {
-        this.address = address;
-    }
-
+    // Atributos
     protected String name;
     protected int age;
     protected String document;
@@ -14,4 +10,65 @@ public class Person
     protected String phone;
     protected String login;
     protected Address address;
+
+    // Construtor
+    protected Person(Address address)
+    {
+        this.address = address;
+    }
+
+    // GET & SET
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public String getDocument()
+    {
+        return document;
+    }
+    public void setDocument(String document)
+    {
+        this.document = document;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public String getLogin()
+    {
+        return login;
+    }
+    public void setLogin(String login)
+    {
+        this.login = login;
+    }
 }
