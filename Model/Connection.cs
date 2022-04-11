3ab0b4f065339c95c363d887de276194497d213e
Bruno Viotto <brunoviottosantos@gmail.com>
2022-04-11 13:17:26 -0300

    Update

--- Model/Connection.cs
+++ Model/Connection.cs
@@ -1,7 +1,26 @@
 namespace Model;
+
 using Microsoft.EntityFrameworkCore;
 
 public class Contexto : DbContext
 {
-     
+    public DbSet<Address> Address { get; set; }
+    public DbSet<Client> Client { get; set; }
+    public DbSet<Owner> Owner { get; set; }
+    public DbSet<Person> Person { get; set; }
+    public DbSet<Product> Product { get; set; }
+    public DbSet<Purchase> Purchase { get; set; }
+    public DbSet<Stocks> Stocks { get; set; }
+    public DbSet<Store> Store { get; set; }
+    public DbSet<WishList> WishList { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source = JVLPC0524; Initial Catalog = marketplace; Integrated Security = True");
+    }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+
+    }
 }
