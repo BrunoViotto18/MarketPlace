b7318bab481fff00d35ef1309644d63df01c9790
Nathan <nathan_cordeiro03@hotmail.com>
2022-04-11 14:03:13 -0300

    Adicionando Classes Address  e Person na solução DBO

--- DAO/Address.cs
+++ DAO/Address.cs
@@ -8,5 +8,11 @@ namespace DAO
 {
     internal class Address
     {
+        public int id;
+        public string city;
+        public string state;
+        public string postal_code;
+        public string country;
+
     }
 }
--- DAO/Person.cs
+++ DAO/Person.cs
@@ -8,5 +8,13 @@ namespace DAO
 {
     internal class Person
     {
+        public int id;
+        public string name;
+        public DateTime date_of_birth;
+        public string phone;
+        public string login;
+        public string password;
+        public string email;
+        public Address address;
     }
 }
