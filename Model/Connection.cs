628494b287d17c94d5a4ceabfbddc94c0c92e420
Nathan <nathan_cordeiro03@hotmail.com>
2022-04-11 13:19:59 -0300

    Update2.0

--- Model/Connection.cs
+++ Model/Connection.cs
@@ -1,7 +1,19 @@
-namespace Model;
-using System.Data.Entity;
+namespace Model
+using Microsoft.EntityFrameworkCore;
 
-public MyDbContext CreateDbContext()
+string myConnectionString = "Data Source=JVLPC0510\SQLSERVER;Initial Catalog=TrevisanDB;Integrated Security=True";
+MyDatabaseEntities entidades = new MyDatabaseEntities(myConnectionString);
+
+public partial class MyDatabaseEntities : DbContext
 {
-     return new MyDbContext(connectionString);
+public MyDatabaseEntities(string connectionString)
+    : base(connectionString)
+{
 }
+
+protected override void OnModelCreating(DbModelBuilder modelBuilder)
+{
+    throw new UnintentionalCodeFirstException();
+}
+
+public virtual DbSet<MyTable> MyTable { get; set; }
