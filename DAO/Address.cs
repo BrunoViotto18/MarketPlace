f8a561824acfd0f8658e2049aec0d2f8c7b9062c
Bruno Viotto <brunoviottosantos@gmail.com>
2022-04-11 14:09:45 -0300

    Criação de parte da camada DAO.

--- DAO/Address.cs
+++ DAO/Address.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DAO;
 
-namespace DAO
+public class Address
 {
-    internal class Address
-    {
-        public int id;
-        public string city;
-        public string state;
-        public string postal_code;
-        public string country;
+    public int id;
+    public string city;
+    public string state;
+    public string postal_code;
+    public string country;
 
-    }
 }
--- DAO/Person.cs
+++ DAO/Person.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DAO;
 
-namespace DAO
+public class Person
 {
-    internal class Person
-    {
-        public int id;
-        public string name;
-        public DateTime date_of_birth;
-        public string phone;
-        public string login;
-        public string password;
-        public string email;
-        public Address address;
-    }
+    public int id;
+    public string name;
+    public DateTime date_of_birth;
+    public string phone;
+    public string login;
+    public string password;
+    public string email;
+    public Address address;
 }
